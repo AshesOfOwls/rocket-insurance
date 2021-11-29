22caa1f7cad146b44be6c2ae9bf35d8460743f38
Quinn Elwood <ashes@quinns-mbp.lan>
2021-11-28 17:46:52 -0800

    Update typography components to match Figma

--- src/components/atoms/Typography/index.tsx
+++ src/components/atoms/Typography/index.tsx
@@ -3,17 +3,19 @@ import classnames from 'classnames';
 import s from './Typography.module.css';
 
 export interface BaseTextProps {
-  Element?: 'p' | 'h1' | 'h2'
+  Element?: 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5'
+  textType?: 'small' | 'labelText' | 'body' | 'buttonText' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5'
   children: React.ReactNode
   bold?: boolean
+  uppercase?: boolean
 }
 
 const BaseText = (props: BaseTextProps) => {
-  const { Element = 'p', children, bold } = props;
+  const { Element = 'p', children, bold, uppercase, textType = 'body' } = props;
 
   return (
     <Element
-      className={classnames({ [s.bold]: bold })}
+      className={classnames([s[textType]], { [s.bold]: bold, [s.uppercase]: uppercase })}
     >
       { children }
     </Element>
@@ -25,15 +27,107 @@ export default BaseText;
 const Body = (props: BaseTextProps) => {
   const { children, ...otherProps } = props;
   
-  return <BaseText Element='p' {...otherProps}>{ children }</BaseText>;
+  return (
+    <BaseText Element='p' {...otherProps} textType="body">
+      { children }
+    </BaseText>
+  );
 };
 
 export { Body };
 
+const LabelText = (props: BaseTextProps) => {
+  const { children, ...otherProps } = props;
+  
+  return (
+    <BaseText Element='p' {...otherProps} textType="labelText">
+      { children }
+    </BaseText>
+  );
+};
+
+export { LabelText };
+
+const Small = (props: BaseTextProps) => {
+  const { children, ...otherProps } = props;
+  
+  return (
+    <BaseText Element='p' {...otherProps} textType="small">
+      { children }
+    </BaseText>
+  );
+};
+
+export { Small };
+
+const ButtonText = (props: BaseTextProps) => {
+  const { children, ...otherProps } = props;
+  
+  return (
+    <BaseText Element='p' {...otherProps} textType="buttonText">
+      { children }
+    </BaseText>
+  );
+};
+
+export { ButtonText };
+
 const H1 = (props: BaseTextProps) => {
   const { children, ...otherProps } = props;
   
-  return <BaseText Element='h1' {...otherProps}>{ children }</BaseText>;
+  return (
+    <BaseText Element='h1' {...otherProps} textType="h1" bold>
+      { children }
+    </BaseText>
+  );
 };
 
 export { H1 };
+
+const H2 = (props: BaseTextProps) => {
+  const { children, ...otherProps } = props;
+
+  return (
+    <BaseText Element='h2' {...otherProps} textType="h2" bold>
+      { children }
+    </BaseText>
+  );
+};
+
+export { H2 };
+
+const H3 = (props: BaseTextProps) => {
+  const { children, ...otherProps } = props;
+
+  return (
+    <BaseText Element='h3' {...otherProps} textType="h3" bold>
+      { children }
+    </BaseText>
+  );
+};
+
+export { H3 };
+
+const H4 = (props: BaseTextProps) => {
+  const { children, ...otherProps } = props;
+
+  return (
+    <BaseText Element='h4' {...otherProps} textType="h4" bold>
+      { children }
+    </BaseText>
+  );
+};
+
+export { H4 };
+
+const H5 = (props: BaseTextProps) => {
+  const { children, ...otherProps } = props;
+
+  return (
+    <BaseText Element='h5' {...otherProps} textType="h5" bold>
+      { children }
+    </BaseText>
+  );
+};
+
+export { H5 };
--- src/components/atoms/Typography/stories.tsx
+++ src/components/atoms/Typography/stories.tsx
@@ -2,7 +2,13 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import Typography, {
   Body as BodyComponent,
-  H1 as H1Component
+  Small as SmallComponent,
+  LabelText as LabelTextComponent,
+  ButtonText as ButtonTextComponent,
+  H1 as H1Component,
+  H2 as H2Component,
+  H3 as H3Component,
+  H4 as H4Component,
 } from '.';
 
 export default {
@@ -21,9 +27,51 @@ Body.args = {
   children: 'Body Text',
 };
 
+const SmallTemplate: ComponentStory<typeof SmallComponent> = (args) => <SmallComponent {...args} />;
+
+export const Small = SmallTemplate.bind({});
+Small.args = {
+  children: 'Small Text',
+};
+
+const LabelTextTemplate: ComponentStory<typeof LabelTextComponent> = (args) => <LabelTextComponent {...args} />;
+
+export const LabelText = LabelTextTemplate.bind({});
+LabelText.args = {
+  children: 'LabelText Text',
+};
+
+const ButtonTextTemplate: ComponentStory<typeof ButtonTextComponent> = (args) => <ButtonTextComponent {...args} />;
+
+export const ButtonText = ButtonTextTemplate.bind({});
+ButtonText.args = {
+  children: 'ButtonText Text',
+};
+
 const H1Template: ComponentStory<typeof H1Component> = (args) => <H1Component {...args} />;
 
 export const Header1 = H1Template.bind({});
 Header1.args = {
   children: 'Header 1',
+};
+
+const H2Template: ComponentStory<typeof H2Component> = (args) => <H2Component {...args} />;
+
+export const Header2 = H2Template.bind({});
+Header2.args = {
+  children: 'Header 2',
+};
+
+const H3Template: ComponentStory<typeof H3Component> = (args) => <H3Component {...args} />;
+
+export const Header3 = H3Template.bind({});
+Header3.args = {
+  children: 'Header 3',
+};
+
+const H4Template: ComponentStory<typeof H4Component> = (args) => <H4Component {...args} />;
+
+export const Header4 = H4Template.bind({});
+Header4.args = {
+  children: 'Header 4',
 };
